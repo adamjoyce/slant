a6177df939b1deb66d039b788a4e710cb7f66b84
Adam Joyce <adamjoyce@outlook.com>
2017-07-20 21:04:34 +0100

    WIP - Terrain generator + recycling pool

---
+++ slant/Assets/Scripts/Monobehaviours/TerrainController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainController : MonoBehaviour
+{
+    public GameObject terrainPrefab;            // The terrain gameobject that is spawned.
+
+    private List<GameObject> terrainPool;       // The pool of existing terrain pieces.
+
+    /* Use this for initialization. */
+    void Start()
+    {
+        terrainPool = new List<GameObject>();
+
+        // Add the starting terrain piece to the pool.
+        GameObject startingTerrain = GameObject.Find("TerrainBasic");
+        terrainPool.Add(startingTerrain);
+    }
+
+    /* Update is called once per frame. */
+    void Update()
+    {
+
+    }
+}
