5f0ed9b9dc7d9441967e8fa1a42ecab188f45a2c
Adam Joyce <adamjoyce@outlook.com>
2017-07-21 16:54:51 +0100

    Switching slant positions working

---
+++ slant/Assets/Scripts/Monobehaviours/PlayerCharacter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    public float maximumHealth = 100.0f;            // Loose all this and death comes.
+
+    private float currentHealth = 0.0f;             // How much health the player currently has.
+
+    /* Use this for initialization. */
+    private void Start()
+    {
+        currentHealth = maximumHealth;
+    }
+
+    /* Update is called once per frame. */
+    private void Update()
+    {
+
+    }
+
+    /* The player dies if they take damage that meets or surpasses their health pool. */
+    public void TakeDamage(float damageAmount)
+    {
+        // Update health and check for death.
+        currentHealth -= damageAmount;
+        if (currentHealth <= 0.0f)
+        {
+            Die();
+        }
+    }
+
+    /* Behaviour thata happens on death. */
+    private void Die()
+    {
+        // Deathy stuff...
+    }
+}
---
+++ slant/Assets/Scripts/Monobehaviours/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform upperPosition;         // The player position above the slant.
+    public Transform lowerPosition;         // The player position below the slant.
+
+    private GameObject player;              // The player.
+    private Animator anim;                  // The player animator.
+    private bool aboveSlant = true;         // Used to switch to above or below the slant.
+
+    /* Use this for initialization. */
+    private void Start()
+    {
+        player = gameObject;
+        anim = player.GetComponent<Animator>();
+    }
+
+    /* Update is called once per frame. */
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (aboveSlant)
+            {
+                // Move to below the slant.
+                player.transform.SetPositionAndRotation(lowerPosition.position, lowerPosition.rotation);
+                anim.SetBool("aboveSlant", false);
+                aboveSlant = false;
+            }
+            else
+            {
+                // Move to above the slant.
+                player.transform.SetPositionAndRotation(upperPosition.position, upperPosition.rotation);
+                anim.SetBool("aboveSlant", true);
+                aboveSlant = true;
+            }
+        }
+    }
+}
